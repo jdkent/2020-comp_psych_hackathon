f7fbe19efc29289186f743988c8586e676baa349
James Kent <james-kent@uiowa.edu>
2020-01-11 23:12:08 -0600

    fix name of environment

--- setup.sh
+++ setup.sh
@@ -3,4 +3,4 @@
 python3 -m venv "${HOME}"/envs/bp_hack
 source "${HOME}"/envs/bp_hack/bin/activate
 pip install -r requirements.txt
-ipython kernel install --user --name=projectname
+ipython kernel install --user --name=bp_hack
